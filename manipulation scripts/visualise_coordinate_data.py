7061b374dec572769111f84924cd0e7320dc78a4
Edan <edan.landow@gmail.com>
2024-09-07 01:11:11 +1000

    small dots look better

--- manipulation scripts/visualise_coordinate_data.py
+++ manipulation scripts/visualise_coordinate_data.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 data = pd.read_excel("../data/Crocodile_Survey_Data_2021_22.xlsx", "Cleaned data")
 
-scatter = pyp.scatter(data["Longitude"], data["Latitude__"])
+scatter = pyp.scatter(data["Longitude"], data["Latitude__"], s=0.1, alpha=1)
 pyp.show()
